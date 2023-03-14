8d07ed332df3e556b381004bea542ff04121d6d0
luciamartinnunez <lucia.martin.nunez@alumnos.upm.es>
2023-03-14 09:58:42 +0100

    "add web greeting controller issue #1"

--- src/main/java/hello/GreetingController.java
+++ src/main/java/hello/GreetingController.java
@@ -8,7 +8,7 @@ public class GreetingController {
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World")
                            String name, Model model) {
-        model.addAttribute("name", name);
+        model.addAttribute("name1", name);
         return "greeting";
     }
 }
